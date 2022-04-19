731d396359918c70739a1f6f69387606492791fb
Brisa-Codes <brisamukunde1@gmail.com>
2022-04-19 10:44:57 +0300

    update: address

--- hardhat-tut/constants/index.js
+++ hardhat-tut/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x0B7cE963da02A533e31c96B50566207145Ff1E5A";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection2-rust.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
